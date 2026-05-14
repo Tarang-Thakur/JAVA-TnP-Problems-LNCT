6878a10952981540592432b448db15d0ef57f3e6
Tarang Thakur <tarangthakur12@gmail.com>
2026-05-14 07:44:59 +0530

    Added Star Pyramid Solution in Pattern Printing

--- Pattern_Printing/StarPyramid.java
+++ Pattern_Printing/StarPyramid.java
@@ -1,8 +1,15 @@
 package Pattern_Printing;
 
 public class StarPyramid {
-
     public static void main(String[] args) {
-        
+        for(int i=0; i<5;i++){
+            for(int j=0;j<5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<2*i+1;j++){
+                System.out.print("*");
+            }     
+            System.out.println(" ");
+        }
     }
 }
