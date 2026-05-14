bb37bdd369b302ed9804e23a25a66d3b681f9996
Tarang Thakur <tarangthakur12@gmail.com>
2026-05-14 07:55:41 +0530

    Added UpsideDownStarPyramid Solution

---
+++ Pattern_Printing/UpsideDownStarPyramid.java
@@ -0,0 +1,15 @@
+package Pattern_Printing;
+
+public class UpsideDownStarPyramid {
+    public static void main(String[] args) {
+        for(int i=4;i>-1;i--){
+            for(int j=0;j<5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
